df45a3607039da4151ca1065b06b94b01a005767
André Luiz <a.luiz.souto@gmail.com>
2018-11-23 16:08:22 -0300

    alteracao em produtividade.

--- src/app/home/produtividade/produtividade.component.ts
+++ src/app/home/produtividade/produtividade.component.ts
@@ -13,43 +13,48 @@ export class ProdutividadeComponent implements OnInit {
   widht: number = 450;
   heigth: number = 300;
 
-  data: any;
+  data = [];
   options = {
-    title: 'Produtividade',
-    pieSliceTextStyle: { color: 'black' },
-    slices: { 0: { color: '#98FB98' }, 1: { color: '#B8860B' } }
+    chart: { title: 'Produtividade' },
+    bar: 'horizontal',
+    animation: { 
+      easing: 'out',
+      startup: true
+    },
+    legend: {
+      position: 'bottom',
+      alignment: 'start'
+    }
   };
 
   constructor(private produtividadeService: ProdutividadeService) { }
 
   @HostListener('window:resize')
-  onresize(){
+  onResize(){
     
     if(this.resize = window.innerWidth <= 1299) {
 
-      console.log("aaqui");
       this.widht = 450;
       this.heigth = 300;
+    } else {
+      this.widht = 850;
+      this.heigth = 500;
     }
+
+    this.resize = false;
     
   }
 
   ngOnInit() {
 
     this.produtividadeService.getDataChart().subscribe(j => {
-      
-      this.data = [
-        ['SENTENCIADOS', j.sents],
-        ['NÃO SENTENCIADOS', j.nSents]
-      ];
-
-      if(this.resize = window.innerWidth < 1299) {
-
-        console.log('aqui');
-        this.widht = 350;
-        this.heigth = 300;
-      }
-    } );
+      j.forEach((c) => {
+        this.data.push([
+          c.anoMeta, c.sents, c.nSents
+        ]);
+      });
+      this.onResize();
+    });
   }
 
 }
